6be2f8087366784f0534b37167e11ea0f487c664
CarlosC074 <carlos.castano74@gmail.com>
2021-10-24 18:42:20 -0700

    Test files have have been added. The files are empty but should be uploaded. Added an experimental code in the index file to see how to navigate menus. The index file's contents will be edited appropriately

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                message: 'You ok? you ok? You okay, Annie?',
+                name: 'mainConfirm'
+            }
+        ])
+    .then((data) => {
+        data.mainConfirm ? console.log("true") : secondMenu();
+    })
+}
+
+function secondMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Was that a mf korn reference?",
+                name: "kornRef",
+            }
+        ])
+
+    .then((data) => {
+        data.kornRef ? console.log("2nd true") : console.log("2nd false");
+    })
+}
+
+mainMenu();
