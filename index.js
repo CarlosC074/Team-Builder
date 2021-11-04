8d266b3999b729f00b1e585b11a9785c126bafd6
CarlosC074 <carlos.castano74@gmail.com>
2021-11-03 21:20:03 -0700

    Added the first menu that prompts input for the manager. The information is indeed appended to the pieces that will be clicked together.

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const htmlFile = require('./lib/htmlPieces');
+
+function firstMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                //I am aware that 'manager's' is gramatically incorrect, but I do not know how to fix it without confusing the code editor
+                message: 'Welcome to the team builder, what is your managers name?',
+                name: 'managerName'
+            },
+            {
+                type: 'input',
+                message: 'What is the managers ID?',
+                name: 'managerId'
+            },
+            {
+                type: 'input',
+                message: 'What is the managers email?',
+                name: 'managerEmail'
+            },
+            {
+                type: 'input',
+                message: 'What is the managers office number?',
+                name: 'managerOfficeNum'
+            }
+        ])
+    
+        .then((data) => {
+            var manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOfficeNum);
+
+            var managerTblRow = `<tr>${manager.getName()}${manager.getEmail()}${manager.getId()}${manager.getOfficeNum()}</tr>`
+
+            htmlFile.managerTblMid = htmlFile.managerTblMid + managerTblRow;
+            
+            mainMenu();
+        })
+}
 
 function mainMenu() {
     inquirer
         .prompt([
             {
-                type: 'confirm',
-                message: 'You ok? you ok? You okay, Annie?',
+                type: 'list',
+                message: 'Add a team member or, if you are finished, select "create File"',
                 name: 'mainConfirm',
-                 
+                choices: [""]
             }
         ])
     .then((data) => {
@@ -31,4 +73,4 @@ function secondMenu() {
     })
 }
 
-mainMenu();
+firstMenu();
